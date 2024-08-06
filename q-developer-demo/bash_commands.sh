80a1d027ec6ef4fcdc8fa1bccb220c82633e95ca
Workshop Participant <participant@workshops.aws>
2024-08-06 10:25:57 +0000

    completed SQL and SQLAlchemy exmaples

--- q-developer-demo/bash_commands.sh
+++ q-developer-demo/bash_commands.sh
@@ -1,8 +1,13 @@
 
+# Activate Python environment
 python3 -m venv .venv
 source .venv/bin/activate
 
-# Fix my SQL code to be compatible with MySQL 8.0 without changing the schema names.
-# How would my code look with the suggested changes?
+# Prompts
+## Fix my SQL code to be compatible with MySQL 8.0 without changing the schema names.
+## How would my code look with the suggested changes?
 
+# SQL example
 sh ./run_sql_query.sh 
+# SQLAlchemy example
+python3 sqlalchemy_demo_mysql_8.0.py 
